f292bbe7fd7fa8422d81f6e808b705de416a16cb
jedelson <edelson.m.jason@gmail.com>
2017-09-04 15:22:50 -0700

    v1.0-fully working
    3 colors 8 rows, answer shown
    ~to-do
    add more colors for more difficulty
    add more rows to compensate
    make colors and rows variable
    add winning congragulations pop up
    hide answer until game has been finished

--- default.js
+++ default.js
@@ -1,13 +1,32 @@
-var timesguessed = 1;
+var timesguessed = 1; //guess row counter
 var currentcolor = "rgb(255, 255, 255)";
-var circleselect = 0
+var circleselect = 0; // targeting circle for color change
+var tinyselect = 0; // targeting tiny circle
+//random number generation for answer key, changes on page load
+//currently 3 numbers add more later
+var key1 = Math.floor((Math.random() * 3) + 1); 
+var key2 = Math.floor((Math.random() * 3) + 1);
+var key3 = Math.floor((Math.random() * 3) + 1);
+var key4 = Math.floor((Math.random() * 3) + 1);
+var key5 = Math.floor((Math.random() * 3) + 1);
+var guessarray = [0,0,0,0,0]; //used for score tracking
+var keyarray = [key1,key2,key3,key4,key5]; //used for score tracking
+var pegscore = [0,0]; // return value
+var colorarray = ["rgb(0, 0, 0)","rgb(0, 0, 0)","rgb(0, 0, 0)","rgb(0, 0, 0)","rgb(0, 0, 0)"]
+
+//ran on button click
+//changes orb color on each guess and calls score function to display how close guess is
 function submitanswer() {
+	//gets color or guess by id
+	//replace with for loop later
 	var color1 = $("#guess1").css("background-color");
 	var color2 = $("#guess2").css("background-color");
 	var color3 = $("#guess3").css("background-color");
 	var color4 = $("#guess4").css("background-color");
 	var color5 = $("#guess5").css("background-color");
-	circleselect = timesguessed*5;
+	guessarray = [color1,color2,color3,color4,color5] // replaces default with actual colors
+	circleselect = timesguessed*5; //pointer for circle
+	//adds color to correct row
 	$("div.circle:eq("+circleselect+")").css("background-color",color1);
 	circleselect+=1;
 	$("div.circle:eq("+circleselect+")").css("background-color",color2);
@@ -17,11 +36,28 @@ function submitanswer() {
 	$("div.circle:eq("+circleselect+")").css("background-color",color4);
 	circleselect+=1;
 	$("div.circle:eq("+circleselect+")").css("background-color",color5);
+	//calls score function returns values as [black,white]
+	pegscore = score(guessarray);
+	//pointer for tiny score circles
+	tinyselect = timesguessed * 5;
+	tinyselect = tinyselect - 5;
+	//changes color for black tiny circles
+	for (var i = 0; i < pegscore[0]; i++) {
+		$("div.tinycircle:eq("+tinyselect+")").css("background-color","black");
+		tinyselect++
+	} 
+	//changes color for white tiny circles
+	for (var i = 0; i < pegscore[1]; i++) {
+		$("div.tinycircle:eq("+tinyselect+")").css("background-color","white");
+		tinyselect++
+	} 
 	timesguessed = timesguessed + 1;
 }
-
+//for guessing row click rotates color between these, currently [red,green,blue]
+//add more colors later for more difficulty
+//add selector for user variable difficulty
 $(document).ready(function(){
-    $(".answer").click(function(){
+    $(".guess").click(function(){
     	currentcolor = $(this).css("background-color");
     	if (currentcolor == "rgb(255, 255, 255)"){
     		$(this).css("background-color" , "rgb(255, 0, 0)");
@@ -37,3 +73,43 @@ $(document).ready(function(){
     	}
     });
 });
+//converts random numbers to there colors
+// add more colors later
+$(document).ready(function(){
+	for (var i = 0; i < keyarray.length; i++) {
+		if(keyarray[i] == 1){
+			colorarray[i] = "rgb(255, 0, 0)"
+		}
+		else if(keyarray[i] == 2){
+			colorarray[i] = "rgb(0, 255, 0)"
+		}
+		else if(keyarray[i]==3){
+			colorarray[i] = "rgb(0, 0, 255)"
+		}
+		$("div.answer:eq("+i+")").css("background-color",colorarray[i]);
+	}
+})
+
+function score(guessed){
+	var whitepeg = 0;//score holder, denotes perfect match
+	var blackpeg =0;//score holder, denotes right color wrong location
+	var scorearray = [0,0,0,0,0];//makes sure each peg only counts for one score
+	for (var i = 0; i < guessed.length; i++) {
+		if (guessed[i] == colorarray[i]){
+			whitepeg ++ //exact match equal white peg
+			scorearray[i] = 1;
+		}
+		else{//if not exact compare to all other pegs
+			for (var x = 0; x < scorearray.length; x++) {
+				if(guessed[i] == colorarray[x])
+					if (scorearray[x]==0) {//check if peg has already been scored
+						blackpeg++ //right color wrong location
+						scorearray[x] = 1;
+						break
+					}
+			}
+		}
+	}
+	var returnscore = [blackpeg,whitepeg]
+	return returnscore;
+}
